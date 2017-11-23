7595f5cb215882419bbcc0b088deb31495151294
cnoss <christian.noss@fh-koeln.de>
2017-11-23 08:56:54 +0100

    Version für Vorlesung aufgeräunt.

--- scripts/linklist.js
+++ scripts/linklist.js
@@ -37,7 +37,6 @@ var data = [{
 
 ];
 
-var template = "<div class='{{type}}'><h3><a href='{{url}}'>{{title}}</a></h3><p>{{info}}</p></div>";
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -47,35 +46,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     for (var i = 0; i < data.length; i++) {
         var obj = data[i];
-        target.innerHTML += "<div class=\"" + obj.type + "\">";
-        target.innerHTML += "<h3><a href=" + obj.url + ">" + obj.name + "</a></h3>";
-        target.innerHTML += "<p>" + obj.info + "</p>";
-        target.innerHTML += "</div>";
+
     }
 
-    // Linklist Template
-    var target = document.querySelector("#linklistTemplate");
-
-    for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        var html = template;
-        html = html.replace(/{{type}}/, obj.type);
-        html = html.replace(/{{title}}/, obj.name);
-        html = html.replace(/{{url}}/, obj.url);
-        html = html.replace(/{{info}}/, obj.info);
-
-        target.innerHTML += html;
-    }
-
-    // Linklist Vue
-    var app4 = new Vue({
-        el: '#linklistVue',
-        data: {
-            todos: [
-                { text: 'Learn JavaScript' },
-                { text: 'Learn Vue' },
-                { text: 'Build something awesome' }
-            ]
-        }
-    })
 });
